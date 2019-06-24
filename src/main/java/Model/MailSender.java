fcbd7371c4b2b1996c7d774d2ecbde1a39c70bf2
Lwandile MKWANAZI <lmkwanaz@c4r7s3.wethinkcode.co.za>
2019-06-24 17:18:43 +0200

    added the jars

---
+++ src/main/java/Model/MailSender.java
@@ -0,0 +1,42 @@
+package Model;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+public class MailSender {
+	
+	public static void emailSender() {
+		
+//		Message message = new MimeMessage(session);
+//		message.setFrom(new InternetAddress("from@gmail.com"));
+//		message.setRecipients(
+//		Message.RecipientType.TO, InternetAddress.parse("to@gmail.com"));
+//		message.setSubject("Mail Subject");
+//	 
+//		MimeBodyPart mimeBodyPart = new MimeBodyPart();
+//		mimeBodyPart.setContent(msg, "text/html");
+//        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
+//        msg.setSubject(SUBJECT);
+//        msg.setContent(BODY,"text/html");
+//
+//		 SendGrid sg = new SendGrid(System.getenv("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"));
+//		 Request request = new Request();
+//		 
+//		 
+//		 try {
+//		      request.setMethod(Method.POST);
+//		      request.setEndpoint("mail/send");
+//		      request.setBody(mail.build());
+//		      Response response = sg.api(request);
+//		      System.out.println(response.getStatusCode());
+//		      System.out.println(response.getBody());
+//		      System.out.println(response.getHeaders());
+//		      
+//		    } catch (IOException ex) {
+//		      throw ex;
+//		    }
+//		
+}
+
+}
